4c719cc8c270273fc85b3a077e361ff46392ba49
amandasuzzanne <amandasuzanne12@gmail.com>
2024-06-19 12:47:19 +0300

    Complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
+
 def write_file(file_name, file_content):
-    pass
+    
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as newfile:
+        newfile.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as newfile:
+        newfile.write(append_content)
 
 def read_file(file_name):
-    pass
+    # textfile = open(f'{file_name}.txt', encoding='utf-8')
+    # print(textfile.read())
+    with open(f'{file_name}.txt', encoding='utf-8') as textfile:
+        for line in textfile:
+            print(line)
+            return line
